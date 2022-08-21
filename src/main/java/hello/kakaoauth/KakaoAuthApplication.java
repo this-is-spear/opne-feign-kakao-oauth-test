6d7f9c9aaaeb7fd59332ad2e4cfc1c9090247b6d
tis <92219795+this-is-spear@users.noreply.github.com>
2022-08-21 22:09:39 +0900

    Init : 초기 설정

---
+++ src/main/java/hello/kakaoauth/KakaoAuthApplication.java
@@ -0,0 +1,13 @@
+package hello.kakaoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/kakaoauth/KakaoAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.kakaoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
